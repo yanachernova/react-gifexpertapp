efef74b19ef05a362b87f8eb67fabccdc1e0ca43
yanachernova <yana.chernova94@gmail.com>
2020-08-31 16:13:54 -0400

    docs agregado

---
+++ docs/precache-manifest.a8b5dececccc5379c823be2343f583e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a794a5878dbb909b3f2dbd96acf1a585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26b8a160247c59cdeb5d",
+    "url": "/static/css/main.e4c5dec9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b8a160247c59cdeb5d",
+    "url": "/static/js/main.e3067faa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
